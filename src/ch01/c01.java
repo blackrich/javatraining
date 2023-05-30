5fc7e1c77d97f335f21de6db69651e985de98b35
leejunho <junho8296@naver.com>
2023-05-30 09:41:03 +0900

    20230530_1

--- src/ch01/c01.java
+++ src/ch01/c01.java
@@ -1,10 +1,9 @@
 package ch01;
 
 public class c01 {
-
 	public static void main(String[] args) {
-	System.out.print("hello, world");
-
+		System.out.print("hello, world");
+		System.out.print(true);
 	}
 
 }
